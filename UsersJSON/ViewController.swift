1d9b0a3889ef660afbed525f74fa3c024fd549b5
Christian Francis <cfcoding@yahoo.com>
2020-12-04 20:32:55 -0330

    Initial Commit

---
+++ UsersJSON/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsersJSON
+//
+//  Created by Christian Elijah on 2020-12-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
